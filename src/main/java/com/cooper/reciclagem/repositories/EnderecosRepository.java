73cdee086d5721300b824465f62c024e32be787a
dedarob <euclidesroberto16@gmail.com>
2024-12-02 01:21:52 -0300

    adicionado logica funcionarios

--- src/main/java/com/cooper/reciclagem/repositories/EnderecosRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cooper.reciclagem.repositories;
-
-import com.cooper.reciclagem.models.Enderecos;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EnderecosRepository extends CrudRepository<Enderecos, Integer> {
-}
